d7475e879b47df48b46f26e6bfc221f81b00aecf
ISHIKASINGH0612 <your_address_email@domain.comishikasingh0612@gmail.com>
2026-01-02 20:11:09 +0530

    addedd all pictures

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,51 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import AppLayout from './Layouts/app-layout';
+import LandingPage from './pages/landing';
+import Onboarding from './pages/onboarding';
+import JobListing from './pages/job-listing'; 
+import JobPage from './pages/job';
+import MyJobs from './pages/my-jobs';
+
+const router=createBrowserRouter(
+  [
+    {
+      element:<AppLayout/>,
+      children:[
+        {path:'/',
+        element:<LandingPage/>},
+
+         {path:'/onboarding',
+        element:<Onboarding/>},
+
+        {path:'/job-listing',
+        element:<JobListing/>},
+
+        {path:'/job',
+        element:<JobPage/>},
+
+        {path:'/my-job',
+        element:<MyJobs/>},
+
+        {path:'/post-job',
+        element:<PostJob/>},
+
+        {path:'/saved-job',
+        element:<SaveJobs/>}
+
+
+        ]
+
+
+    }
+  ]
+)
+
 
 function App()
 {
   return(
-    <div>welcome to my app</div>
+   <RouterProvider router={router}/>
   )
 }
 
---
+++ src/pages/my-jobs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MyJobs= () => {
+  return (
+    <div>
+      myJobs
+    </div>
+  )
+}
+
+export default MyJobs
+
